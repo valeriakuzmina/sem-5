2e20e86d8afea1e723ef084313c098d8fe91e4c3
Valeria Strigutskaya <kuzminarock2304@gmail.com>
2024-07-25 23:10:36 +0300

    pub

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <productDetails />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import productDetails from './components/productDetails.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   productDetails
   }
 }
 </script>
---
+++ src/components/productDetails.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+<div class="product-details">
+  <div  v-for="product in products" :key="product.id">
+    <h1>{{ product.name }}</h1>
+    <p>{{ getFormattedPrice(product.price) }}</p>
+    <p>{{ getAvailabilityStatus(product.available) }}</p>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'productDetails',
+  data() {
+    return {
+      products: [
+       {
+        id: 1,
+        name: 'shirt',
+        price: 99.85,
+        available: false
+      },
+       {
+        id: 2,
+        name: 'skirt',
+        price: 99.99,
+        available: true
+      },
+       {
+        id: 3,
+        name: 'baby',
+        price: 70.50,
+        available: true
+      }
+      ]
+    };
+  },
+   methods: {
+    getFormattedPrice(price) {
+      return `$${price.toFixed(2)}`;
+    },
+    getAvailabilityStatus(available) {
+      return available ? 'Available' : 'Out of stock';
+    }
+  }
+};
+</script>
+
+<style>
+.product-details{
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+}
+</style>
